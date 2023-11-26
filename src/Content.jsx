bb73ee38b4dd7edf8e2452bca7252c56e9bca85c
dqv28 <dovuong020802@gmail.com>
2023-11-26 18:33:39 +0700

    upadate

---
+++ src/Content.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react"
+
+function Content() {
+    const [countdown, setCountdown] = useState(180)
+
+    useEffect(() => {
+
+        const timeId = setInterval(() => {
+            setCountdown(prevState => prevState - 1);
+        }, 1000)
+
+        return () => clearInterval(timeId)
+
+    }, [])
+
+    return (
+        <>
+            <h1>{countdown}</h1>
+        </>
+    )
+}
+
+export default Content
