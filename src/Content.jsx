aa16d69018b236c5e7f93b7e4be7621e7ec9d536
dqv28 <dovuong020802@gmail.com>
2023-11-26 19:03:52 +0700

    update chat app

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,22 +1,68 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react"
 
+const lessons = [
+    {
+        id: 1,
+        name: "ReactJS là gì? Tại sao nên học ReactJS?"
+    },
+    {
+        id: 2,
+        name: "SPA/MPA là gì?"
+    },
+    {
+        id: 3,
+        name: "Arrow functions"
+    }
+]
+
 function Content() {
-    const [countdown, setCountdown] = useState(180)
+    const [lessonId, setLessonId] = useState(1)
+    const [comments, setComments] = useState([])
+
+    const handleComment = ({ detail }) => {
+
+        setComments(prevState => [...prevState, detail])
+        
+    }
 
     useEffect(() => {
 
-        const timeId = setInterval(() => {
-            setCountdown(prevState => prevState - 1);
-        }, 1000)
+        window.addEventListener(`lesson-${lessonId}`, handleComment)
 
-        return () => clearInterval(timeId)
+        return () => {
+            window.removeEventListener(`lesson-${lessonId}`, handleComment)
+        }
 
-    }, [])
+    }, [lessonId])
 
     return (
         <>
-            <h1>{countdown}</h1>
+            <ul>
+                {
+                    lessons.map(lesson => (
+                        <li 
+                            key={lesson.id}
+                            style={{
+                                color: lessonId === lesson.id ? "red" : "#333"
+                            }}
+                            onClick={() => setLessonId(lesson.id)}
+                        >
+                            {lesson.name}
+                        </li>
+                    ))
+                }
+            </ul>
+
+            {
+                comments && comments.map((comment, index) => (
+                    <h4 
+                        key={index}
+                    >
+                        {comment}
+                    </h4>
+                ))
+            }
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,23 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+// Fake comments
+function emitComment (id) {
+  setInterval(() => {
+    window.dispatchEvent(
+      new CustomEvent(`lesson-${id}`, {
+        detail: `Nội dung comment của lesson ${id}`
+      })
+    )
+  }, 2000);
+}
+
+emitComment(1)
+emitComment(2)
+emitComment(3)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
